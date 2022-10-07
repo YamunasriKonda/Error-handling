6621518e55a76772db1ee8aa626c2b8391f8330b
yamunasrikonda <yamuna.jakkam91@gmail.com>
2022-10-07 10:10:42 +0100

    file

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+
+app.listen(4000);
+
+const express = require ('express');
+const app =express();
+
+app.get('/getError', (req, res, next)){
+
+    next(Error('message'));
+}
+
+app.use((err, req, res, next)=> {
+
+    console.log(err.stack);
+    next(err)
+});
+app.listen(4000);
+
+const express = require('express');
+
+const app= express();
+
+app.get('/getError', (req, res, next)=>{
+  next(Error('message'));
+
+}
+
+app.use((err, req, res, next) => {
+
+    console.log(err.stack);
+    next(err);
+}));
+
+app.use((err, req, res, next) =>{
+
+    res.status(500).send(err.message)
+});
+
+app.listen(4000);
